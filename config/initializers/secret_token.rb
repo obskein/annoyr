59416e7db2742d8191b81be515f0adf755b3aa4b
obskein <obskein@gmail.com>
2011-06-03 09:01:48 -0700

    Start a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Annoyr::Application.config.secret_token = 'de0904adce1082a6bd3844c2b8bb8d6cb2588eabcc53196ebaa147838c8876f560a781131c56a18fdd3da5de10b340676cfa00e8fc1dd39a8d4136765928b9ab'
