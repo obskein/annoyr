a2e4e8528ecc08499a847ffaecc51915c8fcb884
rjungemann <roger.jungemann@wildfireapp.com>
2011-06-03 13:40:29 -0700

    Fixed JS issue

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function() {
-  $('.header').live('click', function() {
-    window.location.pathname = '/';
+(function($) {
+  $(function() {
+    $('.header').live('click', function() {
+      window.location.pathname = '/';
+    });
   });
-});
+})(jQuery);
