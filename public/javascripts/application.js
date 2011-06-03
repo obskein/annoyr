329e5c8f6a262006d04854e5251c3e1d1db74efd
rjungemann <roger.jungemann@wildfireapp.com>
2011-06-03 13:19:26 -0700

    Some styling fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $('.header').live('click', function() {
+    window.location.pathname = '/';
+  });
+});
