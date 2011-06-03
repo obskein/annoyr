5d3fd17960ce6b6bf7f0e449502e94f778e90263
obskein <obskein@gmail.com>
2011-06-03 10:17:11 -0700

    Runner now works all the time. Made runner work.

--- app/models/annoying_runner.rb
+++ app/models/annoying_runner.rb
@@ -6,7 +6,9 @@ class AnnoyingRunner
       scheduler = Rufus::Scheduler.start_new
 
       tick!
-      scheduler.cron '0 7 * * *' do
+      # scheduler.cron '0 7 * * *' do
+      scheduler.every '5s' do
+
         # every day at 7:00am
         tick!
       end
@@ -17,7 +19,8 @@ class AnnoyingRunner
   end
 
   def self.tick!
-    puts 'whos on call!?'
     Team.annoy_all!
+    # don't run out of db connections
+    ActiveRecord::Base.clear_reloadable_connections! 
   end
 end
